7cd06af9eb34ad308d6fe503b9b9f7354aebd101
Wei Zhang <wei@uscabinetdepot.com>
2018-08-13 23:07:38 -0400

    Fixed cell clicked event

--- Sources/Main/ViewControllers/MainViewController.swift
+++ Sources/Main/ViewControllers/MainViewController.swift
@@ -24,18 +24,24 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    collectionView?.backgroundColor = UIColor(red: 42/255, green: 57/255,
-        blue: 95/255, alpha: 1)
+    collectionView?.isUserInteractionEnabled = true
+    collectionView?.allowsMultipleSelection = false
+    collectionView?.allowsSelection = true
+
+    collectionView?.backgroundColor = UIColor(red: 42 / 255, green: 57 / 255,
+        blue: 95 / 255, alpha: 1)
 
     navigationItem.title = "Smart WOS"
-    navigationController?.navigationBar.backgroundColor = UIColor(red: 42/255, green: 57/255,
-        blue: 95/255, alpha: 1)
-    navigationController?.navigationBar.barTintColor = UIColor(red: 217/255, green:
-        48/255, blue: 80/255, alpha: 1)
+
+    navigationController?.navigationBar.backgroundColor = UIColor(red: 42 / 255, green: 57 / 255,
+        blue: 95 / 255, alpha: 1)
+    navigationController?.navigationBar.barTintColor = UIColor(red: 217 / 255, green:
+    48 / 255, blue: 80 / 255, alpha: 1)
 
     navigationController?.navigationBar.titleTextAttributes = [
-      NSForegroundColorAttributeName:  UIColor.white,
+      NSForegroundColorAttributeName: UIColor.white,
       NSFontAttributeName: UIFont.boldSystemFont(ofSize: 20)]
+
 
     collectionView?.register(TeamCell.self, forCellWithReuseIdentifier: cellId)
 
@@ -55,41 +61,39 @@ class MainViewController: UICollectionViewController, UICollectionViewDelegateFl
 
     cell.team = teams[indexPath.item]
 
-    // TODO: double-check if the following incurs memory leak
-    cell.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap(_:))))
-
     return cell
   }
 
-//  override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-//    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-//        as! TeamCell
-//
-//    guard let name = teams[indexPath.item].name else { return }
-//
-//    print("Got clicked on cell name: \(name)!")
-//  }
+  // This is where we handle cell clicked event
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:
+      IndexPath) {
+
+    guard let name = teams[indexPath.item].name else {
+      return
+    }
+
+    print("Got clicked on cell name: \(name)!")
+  }
 
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: (view.frame.width / 3) - 16, height: 100)
   }
 
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-    return UIEdgeInsets(top:8, left: 8, bottom: 8, right: 8)
+    return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
   }
 
-  func tap(_ sender: UITapGestureRecognizer) {
-
-    let location = sender.location(in: self.collectionView)
-    let indexPath = self.collectionView?.indexPathForItem(at: location)
-
-    if let index = indexPath {
-      print("Got clicked on index: \(index)!")
-      let team = teams[index[1]]
-      let name = team.name!
-      print("Got clicked on name: \(name)!")
-    }
+  override func didReceiveMemoryWarning() {
+    let alert = UIAlertController(title: "Memory Alert", message: "Low memory. Restart your " +
+        "device ASAP",
+        preferredStyle:
+        .alert)
+    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+      NSLog("Low memory warning.")
+    }))
+    self.present(alert, animated: true, completion: nil)
   }
+
 }
 
 
@@ -99,8 +103,12 @@ class TeamCell: UICollectionViewCell {
 
     didSet {
 
-      guard let teamImage = team?.image else { return }
-      guard let teamName = team?.name else { return }
+      guard let teamImage = team?.image else {
+        return
+      }
+      guard let teamName = team?.name else {
+        return
+      }
 
       teamImageView.image = UIImage(named: teamImage)
       teamLabel.text = teamName
@@ -114,7 +122,7 @@ class TeamCell: UICollectionViewCell {
     setCellShadow()
   }
 
-  func setCellShadow(){
+  func setCellShadow() {
     self.layer.shadowColor = UIColor.black.cgColor
     self.layer.shadowOffset = CGSize(width: 0, height: 1)
     self.layer.shadowOpacity = 1
@@ -124,19 +132,19 @@ class TeamCell: UICollectionViewCell {
   }
 
   func setup() {
-    self.backgroundColor = UIColor(red: 11/255, green: 22/255, blue: 53/255, alpha: 1)
+    self.backgroundColor = UIColor(red: 11 / 255, green: 22 / 255, blue: 53 / 255, alpha: 1)
     self.addSubview(teamImageView)
     self.addSubview(teamLabel)
 
     teamImageView.anchor(top: topAnchor, left: leftAnchor,
         bottom: nil, right: rightAnchor,
-        paddingTop:10, paddingLeft: 10,
+        paddingTop: 10, paddingLeft: 10,
         paddingBottom: 0, paddingRight: 10,
         width: 0, height: 50)
 
     teamLabel.anchor(top: teamImageView.bottomAnchor, left: leftAnchor,
         bottom: bottomAnchor, right: rightAnchor,
-        paddingTop:0, paddingLeft: 0,
+        paddingTop: 0, paddingLeft: 0,
         paddingBottom: 0, paddingRight: 0,
         width: 0, height: 0)
 
